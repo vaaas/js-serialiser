d66a5ac2e46c0eff806247f011524284cf6d6dc6
Vasileios Pasialiokis <pasivasi@outlook.com>
2022-01-24 13:25:42 +0200

    implement pretty print

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export function serialise(x) {
+export function serialise(x, depth=0) {
     switch (typeof x) {
         case 'number':
             return x.toString()
@@ -12,11 +12,17 @@ export function serialise(x) {
             return 'undefined'
         case 'object':
             if (x === 'null') return 'null'
-            else if (Array.isArray(x)) return serialise_list(x)
+            else if (Array.isArray(x)) return serialise_list(x, depth)
             else throw 'unsupported type: ' + typeof x
         default:
             throw 'unsupported type: ' + typeof x
     }
+}
+
+function make_string(c, n=1) {
+    let s = ''
+    for (let i = 0; i<n; i++) s += c
+    return s
 }
 
 function map_pair(f, x) {
@@ -26,138 +32,149 @@ function map_pair(f, x) {
     return xs
 }
 
-function serialise_list(x) {
+function serialise_list(x, depth) {
     const first = x[0]
     const rest = x.slice(1)
     if (Array.isArray(first))
-        return serialise_nested(serialise_list(first), rest)
+        return serialise_nested(serialise_list(first), rest, depth)
     if (typeof first !== 'string')
         throw 'unexpected value: ' + typeof x
     switch (first) {
         case 'object':
-            return serialise_object(rest)
+            return serialise_object(rest, depth)
         case 'array':
-            return serialise_array(rest)
+            return serialise_array(rest, depth)
         case 'Map':
-            return serialise_Map(rest)
+            return serialise_Map(rest, depth)
         case 'Set':
-            return serialise_Map(rest)
+            return serialise_Sap(rest, depth)
         case 'str':
-            return serialise_string(rest)
+            return serialise_string(rest, depth)
         case '=':
-            return serialise_set(rest)
+            return serialise_set(rest, depth)
         case '==':
-            return serialise_eq(rest)
+            return serialise_eq(rest, depth)
         case '!=':
-            return serialise_neq(rest)
+            return serialise_neq(rest, depth)
         case '===':
-            return serialise_is(rest)
+            return serialise_is(rest, depth)
         case '!==':
-            return serialise_isnt(rest)
+            return serialise_isnt(rest, depth)
         case '>':
-            return serialise_gt(rest)
+            return serialise_gt(rest, depth)
         case '>=':
-            return serialise_gte(rest)
+            return serialise_gte(rest, depth)
         case '<':
-            return serialise_lt(rest)
+            return serialise_lt(rest, depth)
         case '<=':
-            return serialise_lte(rest)
+            return serialise_lte(rest, depth)
         case '&&':
-            return serialise_and(rest)
+            return serialise_and(rest, depth)
         case '||':
-            return serialise_or(rest)
+            return serialise_or(rest, depth)
         case '??':
-            return serialise_null(rest)
+            return serialise_null(rest, depth)
         case '?':
-            return serialise_ternary(rest)
+            return serialise_ternary(rest, depth)
         case '=>':
-            return serialise_arrow(rest)
+            return serialise_arrow(rest, depth)
         default:
-            return serialise_user_function(first, rest)
+            return serialise_user_function(first, rest, depth)
     }
 }
 
-function serialise_user_function(name, args) {
-    return name + '(' + args.map(serialise).join(', ') + ')'
+function serialise_user_function(name, args, depth) {
+    return name + '(' + args.map(x => serialise(x, depth)).join(', ') + ')'
 }
 
-function serialise_array(xs) {
-    return '[' + xs.map(serialise).join(', ') + ']'
+function serialise_array(xs, depth) {
+    return '[\n' +
+        xs.map(x => make_string('\t', depth+1) + serialise(x, depth+1)).join(',\n') +
+        '\n' + make_string('\t', depth) + ']'
 }
 
-function serialise_object(xs) {
-    return '{' + map_pair((k, v) => k + ': ' + v, xs.map(serialise)).join(', ') + '}'
+function serialise_object(xs, depth) {
+    return '{\n' +
+        map_pair((k, v) => make_string('\t', depth+1) + k + ': ' + v, xs.map(x => serialise(x, depth+1))).join(',\n') +
+    '\n' + make_string('\t', depth) + '}'
 }
 
-function serialise_Map(xs) {
-    return 'new Map([' + map_pair((k, v) => '[' + k + ', ' + v + ']', xs.map(serialise)) + '])'
+function serialise_Map(xs, depth) {
+    return 'new Map([\n' +
+        map_pair(
+            (k, v) => make_string('\t', depth+1) + '[' + k + ', ' + v + ']',
+            xs.map(x => serialise(x, depth+1))
+        ).join(',\n') +
+        '\n' + make_string('\t', depth) + '])'
 }
 
-function serialise_infix(s, xs) {
-    return xs.map(serialise).join(' ' + s + ' ')
+function serialise_infix(s, xs, depth) {
+    return xs.map(x => serialise(x, depth)).join(' ' + s + ' ')
 }
 
-function serialise_Set(xs) {
-    return 'new Set([' + xs.map(serialise).join(', ') + '])'
+function serialise_Set(xs, depth) {
+    return 'new Set([\n' +
+        xs.map(x => make_string('\t', depth+1) + serialise(x, depth+1)).join(', ') +
+        '\n])'
 }
 
-function serialise_set(xs) {
-    return serialise_infix('=', xs)
+function serialise_set(xs, depth) {
+    return serialise_infix('=', xs, depth)
 }
 
-function serialise_eq(xs) {
-    return serialise_infix('==', xs)
+function serialise_eq(xs, depth) {
+    return serialise_infix('==', xs, depth)
 }
 
-function serialise_neq(xs) {
-    return serialise_infix('!=', xs)
+function serialise_neq(xs, depth) {
+    return serialise_infix('!=', xs, depth)
 }
 
-function serialise_is(xs) {
-    return serialise_infix('===', xs)
+function serialise_is(xs, depth) {
+    return serialise_infix('===', xs, depth)
 }
 
-function serialise_isnt(xs) {
-    return serialise_infix('!==', xs)
+function serialise_isnt(xs, depth) {
+    return serialise_infix('!==', xs, depth)
 }
 
-function serialise_gt(xs) {
-    return serialise_infix('>', xs)
+function serialise_gt(xs, depth) {
+    return serialise_infix('>', xs, depth)
 }
 
-function serialise_gte(xs) {
-    return serialise_infix('>=', xs)
+function serialise_gte(xs, depth) {
+    return serialise_infix('>=', xs, depth)
 }
 
-function serialise_lt(xs) {
-    return serialise_infix('<', xs)
+function serialise_lt(xs, depth) {
+    return serialise_infix('<', xs, depth)
 }
 
-function serialise_lte(xs) {
-    return serialise_infix('<=', xs)
+function serialise_lte(xs, depth) {
+    return serialise_infix('<=', xs, depth)
 }
 
-function serialise_and(xs) {
-    return serialise_infix('&&', xs)
+function serialise_and(xs, depth) {
+    return serialise_infix('&&', xs, depth)
 }
 
-function serialise_or(xs) {
-    return serialise_infix('||', xs)
+function serialise_or(xs, depth) {
+    return serialise_infix('||', xs, depth)
 }
 
-function serialise_null(xs) {
-    return serialise_infix('??', xs)
+function serialise_null(xs, depth) {
+    return serialise_infix('??', xs, depth)
 }
 
-function serialise_ternary(xs) {
+function serialise_ternary(xs, depth) {
     return serialise(xs[0]) + '?' + serialise(xs[1]) + ':' + serialise(xs[2])
 }
 
-function serialise_string(xs) {
+function serialise_string(xs, depth) {
     return '"' + xs.join(' ').replaceAll('\\', '\\\\').replaceAll('"', '\\"') + '"'
 }
 
-function serialise_arrow(xs) {
+function serialise_arrow(xs, depth) {
     const args = xs[0]
     const body = xs.slice(1)
     let str = ''
@@ -169,6 +186,6 @@ function serialise_arrow(xs) {
     if (body.length === 1)
         str += serialise(body[0])
     else
-        str += '{ ' + body.map(serialise).join('\n') + ' }'
+        str += '{ ' + body.map(x => serialise(x, depth)).join('\n') + ' }'
     return str
 }
--- test.js
+++ test.js
@@ -1,3 +1,15 @@
 import { serialise } from './index.js'
 
-console.log(serialise(['object', 'a', 1]))
+console.log(
+    serialise(
+        ['Map',
+            '18239', ['object',
+                'sizes', ['array', ['str', 'sm'], ['str', 'md']],
+                'color', ['str', 'blue']],
+
+            '914', ['object',
+                'sizes', ['array', ['str', 'md'], ['str', 'lg']],
+                'color', ['str', 'red']]
+        ]
+    )
+)
